b024e410ffa67bbbf6ff8e50506b40344bb537c9
Harshit Singh Arya <136834314+Harsh0906i@users.noreply.github.com>
2024-02-26 18:42:06 +0530

    Update verifyUser.js

--- utils/verifyUser.js
+++ utils/verifyUser.js
@@ -2,7 +2,7 @@ const errorHandler = require('./error')
 const jwt = require('jsonwebtoken');
 
 function verifyUser(req, res, next) {
-    const token = req.cookies;
+    const token = req.cookies.access_token;
    next(errorHandler(403,token))
 };
 
