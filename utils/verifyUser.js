f5c06a17a7dff9ff40b5871e4a0feb804be3885a
Harshit Singh Arya <136834314+Harsh0906i@users.noreply.github.com>
2024-02-26 18:34:19 +0530

    Update verifyUser.js

--- utils/verifyUser.js
+++ utils/verifyUser.js
@@ -2,18 +2,8 @@ const errorHandler = require('./error')
 const jwt = require('jsonwebtoken');
 
 function verifyUser(req, res, next) {
-    const token = res.Cookies.access_token;
-    if (!token) {
-        return next(errorHandler(401,'Unauthorised'))
-    }
-
-    jwt.verify(token,"cnbfR@@^bsbsdbsbg$@", (err, user) => {
-        if (err) {
-            return next(errorHandler(403,'Forbidden'))
-        }
-        req.user = user;
-        next();
-    });
+    const token = req.cookies;
+    res.send(token)
 };
 
 module.exports = verifyUser;
