c3e5f9a02af7215263f241830205b09207483085
Harshit Singh Arya <136834314+Harsh0906i@users.noreply.github.com>
2024-02-26 21:10:22 +0530

    Update User.js

--- routes/User.js
+++ routes/User.js
@@ -47,10 +47,11 @@ router.get('/listing/:id', verifyUser,async (req, res, next) => {
             const listings = await Listing.find({ userRef: req.params.id });
             res.status(200).json(listings);
         } catch (error) {
-           res.send('error');
+                    next(error);
+
         }
     } else {
-        res.status(401).json('not allowed');
+          return next(errorHandeler(401, 'You can only view your own listings!'));
     }
 });
 
