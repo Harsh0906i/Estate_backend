89ea21a84df4e00093910e0df5bcfac2b3881957
Harshit Singh Arya <136834314+Harsh0906i@users.noreply.github.com>
2024-02-26 17:52:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ const cookieParser = require('cookie-parser');
 const path = require('path');
 const cors=require('cors');
 const session=require('express-session');
+app.use(cors())
 app.use(session({
     secret:"dfkjnkjfjdb"
 }));
-app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
