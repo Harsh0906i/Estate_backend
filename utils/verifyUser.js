801394e4fe3509a8211eabe04446449754c8a725
Harshit Singh Arya <136834314+Harsh0906i@users.noreply.github.com>
2024-02-26 18:59:35 +0530

    Update verifyUser.js

--- utils/verifyUser.js
+++ utils/verifyUser.js
@@ -3,10 +3,17 @@ const jwt = require('jsonwebtoken');
 
 function verifyUser(req, res, next) {
     // Access the full cookie object from req.cookies
-    const cookies = req.cookies;
-
-    // Respond with the full cookie object
-    res.status(200).json(cookies);
+    const token = res.cookies.access_token;
+if (!token) {
+        return next(errorHandler(401,'Unauthorised'))
+    }
+ jwt.verify(token,"cnbfR@@^bsbsdbsbg$@", (err, user) => {
+        if (err) {
+            return next(errorHandler(403,'Forbidden'))
+        }
+        req.user = user;
+        next();
+    });
 }
 
 module.exports = verifyUser;
