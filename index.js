dc09c7d556f85c3397aada17c24fd8aa5caf6dbe
Harshit <harshitsingharya24@gmail.com>
2025-04-01 16:15:02 +0530

    updated

--- index.js
+++ index.js
@@ -10,7 +10,12 @@ const ListingRoute = require('./routes/listing');
 const cookieParser = require('cookie-parser');
 const path = require('path');
 const cors=require('cors');
-app.use(cors());
+app.use(cors({
+    origin: 'https://fullstack-mern-estate-ldky.vercel.app', // Change this to the frontend URL
+    credentials: true, // Allow credentials like cookies
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+  }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
